67811245310bac1da5a985850c555708fc5c7721
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-08-23 18:45:02 -0300

    Feito a pagina home listando os pokemons nos cards com imagens e nomes

---
+++ src/Hooks/useRequestData.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from  "react";
+import axios from "axios";
+
+
+export const useRequestData = (initialValue, url) => {
+    const [data, setData] = useState(initialValue);
+    useEffect(() => {
+        axios.get(url)
+            .then((res) => {
+                setData(res.data)
+            })
+            .catch((err) => {
+                alert("Erro ao acessar endpoint, tente novamente");
+            });
+    }, [url]);
+    return [data, setData]
+}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,32 @@
-import {Button} from "@material-ui/core";
+import {Button, Grid} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
 import {goToMyPokedex, goToPokemonDetails} from "../../routes/coordinator";
+import { useRequestData } from "../../Hooks/useRequestData";
+import { BASE_URL } from "../../constants/url";
+import PokemonCard from "../../Components/Cards/PokemonCard";
 
 
 const HomePage = () => {
     const history = useHistory()
+    const [data] = useRequestData({}, `${BASE_URL}?limit=20&offset=20`)
 
+    const renderListaPokemon = data.results ? data.results.map((pokemon) => {
+
+        return <Grid item xs={3} >
+            <PokemonCard 
+                name={pokemon.name}
+                url={pokemon.url}
+            />
+        </Grid>
+        
+    }) : <p>Carregando...</p>
     return (
     <>
         <h1>HomePage</h1>
+        
+        <Grid container spacing={4} style={{padding:'20px'}}>
+            {renderListaPokemon}  
+        </Grid>
         <Button variant={"contained"} color={"primary"} onClick={() => goToMyPokedex(history)}>Pokedex</Button>
         <Button variant={"contained"} color={"primary"} onClick={() => goToPokemonDetails(history)}>Detalhes do Pokemon</Button>
     </>
